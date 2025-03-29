919028a5fcfb177c2b7d22bf3c15e50dfab87e91
Sandipan Rakshit <sandipanrakshit6@gmail.com>
2025-03-29 23:15:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : '/Marketing-Website/',
+  base : '/Workstation-Portfolio-agency/',
   plugins: [react()],
 })
